6ae37a6d2277ce853e5ae73b0a1afe2667859176
mit <mit945.mc@gmail.com>
2020-12-06 20:51:22 -0800

    first

---
+++ src/build/precache-manifest.13061a58db2f295e821687c0ee8dcf44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b60767146f7a7972ae850275a28a9c82",
+    "url": "/static/media/pp.b6076714.png"
+  },
+  {
+    "revision": "8fc6a11c1e26d66249ff70d1da9e1d92",
+    "url": "/static/media/USATT.8fc6a11c.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d4c625221377268d79c",
+    "url": "/static/js/main.47c41487.chunk.js"
+  },
+  {
+    "revision": "64c596c50024e8883255",
+    "url": "/static/js/2.79e74b41.chunk.js"
+  },
+  {
+    "revision": "2d4c625221377268d79c",
+    "url": "/static/css/main.93a7f43d.chunk.css"
+  },
+  {
+    "revision": "581f74eaffdc0cc01f06819ba5d48429",
+    "url": "/index.html"
+  }
+];
